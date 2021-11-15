1907173c4edb84dd79112be0d752807ec084ba08
gekishou <gonfuentes112@gmail.com>
2021-11-15 15:11:26 -0300

    TP6

---
+++ TP6/Data/LibroContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TP6.Models;
+
+namespace TP6.Data
+{
+    public class LibroContext : DbContext
+    {
+        public LibroContext (DbContextOptions<LibroContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TP6.Models.Libro> Libro { get; set; }
+    }
+}
---
+++ TP6/Models/Libro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP6.Models
+{
+    public class Libro
+    {
+        [Key] //clave primaria
+        public int IdLibro { get; set; }
+        [Required] //todos los campos son requeridos
+        public String Titulo { get; set; }
+        [Required]
+        public String Isbn { get; set; }
+        [Required]
+        public String Autor { get; set; }
+
+        [Required]
+        public String Lugar { get; set; }
+        [Required]
+        public int Paginas { get; set; }
+        [Required]
+        public int Edicion { get; set; }
+        [Required]
+        public String Editorial { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+
+    }
+
+}
